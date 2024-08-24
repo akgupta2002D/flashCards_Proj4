fc2d8a663ba73ed4b0a04651d58cf5e482a4f5a2
akgupta2002D <akgupta2002d@gmail.com>
2024-08-24 12:27:36 +0545

    Added Navbar and Clerk Implementation from teammates work. Finished Product

--- app/page.js
+++ app/page.js
@@ -1,21 +1,29 @@
-import { SignInButton, SignedIn, SignedOut, UserButton, RedirectToSignIn } from "@clerk/nextjs";
-import { Box, Button, Typography } from "@mui/material";
-import Image from "next/image";
+'use client'
 
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  RedirectToSignIn
+} from '@clerk/nextjs'
+import { Box, Button, Typography } from '@mui/material'
+import Image from 'next/image'
+import { Navbar } from './baazil_work/components/Navbar'
+import DocumentUploader from './ankit_work/components/DocumentUploader' // Adjust the path as necessary
 // For each route, create a folder inside app with the name you want your route to be, and then a file called 'page.js' inside that folder.
 
-
-export default function Home() {
+export default function Home () {
   return (
-   /* This is the home route. SignedIn component displays the content shown when the user is signed in. Same goes for SignedOut */
-   <>
-    <SignedIn> 
-      <Typography variant="h3"> Welcome to Flashcard SaaS</Typography>
-    </SignedIn>
-    <SignedOut>
-      <RedirectToSignIn />
-    </SignedOut>
-   
-   </>
-  );
+    /* This is the home route. SignedIn component displays the content shown when the user is signed in. Same goes for SignedOut */
+    <>
+      <SignedIn>
+        <Navbar />
+        <DocumentUploader />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
+  )
 }
